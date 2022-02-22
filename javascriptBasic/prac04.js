1f5e396d3ca1292707d8f6e2d98d7a534b6db1d0
YYTAEHWAN <ygen1002@gmail.com>
2022-02-22 13:25:57 +0900

    for 특이한 부분만

--- javascriptBasic/prac04.js
+++ javascriptBasic/prac04.js
@@ -3,7 +3,7 @@ let car02 = "bmw";
 let car03 = "tesla";
 
 let cars = [car01, car02, 392];
-console.log(cars);
+//console.log(cars);
 
 //in Java
 //String [] cars = new array[3];
@@ -12,4 +12,12 @@ console.log(cars);
 // arrayList를 활용하여 데이터를 표현하고
 // object형태로 데이터를 받아올 것임
 // 그 데이터를 다음에 배우는 반복문을 통해 다뤄볼 것임
-console.log(cars[1])
+//console.log(cars[1])
+
+for (car of cars) {
+    console.log(car)
+}
+
+cars.map((car) => {
+    console.error(car);
+});
