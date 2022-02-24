465b11d183b29e0d2dd317d1d357e1c2c8cfb69d
YYTAEHWAN <ygen1002@gmail.com>
2022-02-24 11:13:07 +0900

    AccessToken 요청에서 흥미로웠던 query-string parsing

--- fintech/src/App.js
+++ fintech/src/App.js
@@ -3,6 +3,11 @@ import ListComponent from "./components/ListComponent";
 import Welcome from "./components/Welcome";
 import AxiosTest from "./pages/AxiosTest";
 import NewsApiPage from "./pages/NewsApiPage";
+import BitcoinAPI from "./components/0223Prac/BitcoinAPI";
+import Practice from "./components/0223Prac/Practice";
+import AuthPage from "./pages/AuthPage";
+import AuthResult from "./pages/AuthResult";
+
 
 function App() {
   return (
@@ -11,14 +16,26 @@ function App() {
         <Route path="/test" element={<ListComponent />}></Route>
       </Routes>
       <Routes>
-        <Route path="/" element={<Welcome />}></Route>
-      </Routes>
-      <Routes>
         <Route path="/axiosTest" element={<AxiosTest />}></Route>
       </Routes>
       <Routes>
         <Route path="/news" element={<NewsApiPage />}></Route>
       </Routes>
+      <Routes>
+        <Route path="/" element={<AuthPage ></AuthPage>}></Route>
+      </Routes>
+      <Routes>
+        <Route path="/authResult" element={<AuthResult ></AuthResult>}></Route>
+      </Routes>
+
+      {/* 실습코드 */}
+      <Routes>
+        <Route path="/practice" element={<Practice feeling="happy!"></Practice>}></Route>
+      </Routes>
+      <Routes>
+        <Route path="/bitcoinapi" element={<BitcoinAPI></BitcoinAPI>}></Route>
+      </Routes>
+
     </BrowserRouter>
   );
 }
